910b010ef47088683c657f163e2523bb24f490ea
Andrew Novykov <papapin777@gmail.com>
2020-02-13 03:53:07 -0800

    Get Current User Profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+
+//@route Post api/profile/me
+//@desc get user  user
+//@access Private
+
 router.get("/", (req, res) => res.send("Post route"));
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,32 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Profile route"));
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@route Post api/profile/me
+//@desc get user  user
+//@access Private
+
+
+router.get("/me", auth, async (req, res) => {
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({msg: 'there is no profile for this user'})
+        }
+
+        res.json(profile);
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error')
+    }
+});
 
 module.exports = router;
